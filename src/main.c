64a64269b969c5197675656ac7a20bffce7ba93f
kvnen <koivunen.anttisakari@gmail.com>
2025-04-11 15:19:41 +0300

    did pwm

--- src/main.c
+++ src/main.c
@@ -1,33 +1,53 @@
-
+#include <stdbool.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-// has to be volatile so the compiler doesnt fuck it up
-volatile uint16_t timer_counter = 0;
-//interrupt service routine when timer 0 comparator A is reached
-ISR(TIMER0_COMPA_vect){
-	timer_counter++;
+volatile unsigned long timesec = 0;
+ISR(TIMER1_COMPA_vect){
+	timesec++;
+}
+void inittimer(){
+	TCCR1A = 0b00000000;
+	TCCR1B = 0b00001011;
+	OCR1A = 249;
+	TIMSK1 = 0x02;
+}
+void initpwm(){
+	TCCR0A = 0b10000011;
+	TCCR0B = 0b00000100;
+	OCR0A = 0;
+	TIMSK0 = 0x00;
+	DDRD |= (1 << PB6);
+}
+void delay(unsigned short x){
+	unsigned long newtime = timesec + x;
+	while(1){
+			if(timesec < newtime){
+				continue;
+			}else{
+				return;
+			}
+		}
 }
 int main(void){
-	//set the timer to CTC so that the timer is cleared when it reaches a comparison?
-	TCCR0A |= 0x02;
-	//set the prescaler to divide by 64
-	TCCR0B |= 0x03;
-	//set the comparison A to 250 so that it is met every 1ms
-	OCR0A |= 0xFA;
-	//set comparison A enable
-	TIMSK0 |= (1 << OCIE0A);
-	//set all Port B to output 
-	DDRB |= 0xFF;
-	//set portb4 high so that when the timer counter reaches 500 it switches between the 2
-	PORTB = 0x10;
-	//set enable interrupts to enable global interrupts
+	inittimer();
+	initpwm();
 	sei();
-	//just some basic shit ya know ya know
+	bool leddir = true;
 	while(1){
-			if (timer_counter >= 500){
-				timer_counter = 0;
-				PORTB ^= 0x30;
-					
-			}
+		delay(10);
+		if(OCR0A <= 2)
+		{
+			leddir = true; 
+		}
+		else if(OCR0A >= 254)
+		{
+			leddir = false;
+		}
+		if(leddir == true)
+		{
+			OCR0A++;
+		}else{
+			OCR0A--;
+		}
 		}
 }
